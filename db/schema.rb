ee1cbb9714cf452b512a7d164db5d44ed4b15e74
Tejaswini <tejaswini@chatwoot.com>
2021-11-11 15:12:47 +0530

    Feat: Save automation rules

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_112607) do
+ActiveRecord::Schema.define(version: 2021_11_10_101046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -121,6 +121,18 @@ ActiveRecord::Schema.define(version: 2021_11_22_112607) do
     t.datetime "updated_at", null: false
     t.string "fallback_title"
     t.string "extension"
+  end
+
+  create_table "automation_rules", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.string "name"
+    t.text "description"
+    t.string "event_name"
+    t.jsonb "conditions"
+    t.jsonb "actions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_automation_rules_on_account_id"
   end
 
   create_table "campaigns", force: :cascade do |t|
@@ -320,7 +332,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_112607) do
     t.datetime "agent_last_seen_at"
     t.jsonb "additional_attributes", default: {}
     t.bigint "contact_inbox_id"
-    t.uuid "uuid", default: -> { "public.gen_random_uuid()" }, null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.string "identifier"
     t.datetime "last_activity_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.bigint "team_id"
